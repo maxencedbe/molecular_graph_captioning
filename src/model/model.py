8972dc1c2d3e20e62d5f15d358a163b7af86c021
GAD-cell <gadsinoue@gmail.com>
2025-12-08 21:43:05 +0100

    update model

--- src/model/model.py
+++ src/model/model.py
@@ -11,13 +11,42 @@ node_feat_dim = 177
 edge_feat_dim = 30
 hidden_dim = 512
 projection_dim = 768
+hiddden_dim_n = 512
+hidden_dim_e = 128
+
+class MolEncoder(nn.Module):
+    def __init__(self, hidden_dim_n=512,hidden_dim_e=hidden_dim_e):
+        super(MolEncoder, self).__init__()
+        
+        self.feat_dims = [119, 9, 11, 12, 9, 5, 8, 2, 2] 
+        self.edge_dims = [22,  6, 2] 
+        self.embeddings = nn.ModuleList([
+            nn.Embedding(dim, hidden_dim) for dim in self.feat_dims
+        ])
+
+        self.embeddings_e = nn.ModuleList([
+            nn.Embedding(dim, hidden_dim_e) for dim in self.feat_dims
+        ])
+
+    def forward(self, x, edge_attr):
+        x_embedding = 0
+        edge_emb = 0
+        for i in range(x.size(1)):
+            x_embedding += self.embeddings[i](x[:, i])
+        
+        for i in range(edge_attr.size(1)):
+            edge_emb += self.embeddings_e[i](edge_attr[:, i])
+
+        
+        return x_embedding, edge_emb
+
 
 
 class MessagePassing(MessagePassing):
-    def __init__(self, in_channels, out_channels, dropout=0.0):
+    def __init__(self, in_channels, out_channels, dropout=0.1):
         super(MessagePassing, self).__init__(aggr='add', flow='source_to_target')
 
-        mlp_in_dim = in_channels + edge_feat_dim 
+        mlp_in_dim = in_channels + hidden_dim_e 
 
         self.mlp_message = nn.Sequential(
             nn.Linear(mlp_in_dim, out_channels),
@@ -41,10 +70,10 @@ class MessagePassing(MessagePassing):
         update_input = torch.cat([x, aggr_out], dim=-1)
         return self.mlp_update(update_input)
 
-    
+
 
 class GEncoder(nn.Module):
-    def __init__(self, num_layers=6, in_dim=node_feat_dim, hidden_dim=hidden_dim, dropout=0.1):
+    def __init__(self, num_layers=3, in_dim=node_feat_dim, hidden_dim=hidden_dim, dropout=0.1):
         super(GEncoder, self).__init__()
 
         self.layers = nn.ModuleList()
@@ -52,10 +81,7 @@ class GEncoder(nn.Module):
         self.ffn = nn.ModuleList()
         self.dropout_layer = nn.Dropout(dropout)
 
-        if in_dim != hidden_dim:
-            self.input_proj = nn.Linear(in_dim, hidden_dim)
-        else:
-            self.input_proj = nn.Identity()
+        self.input_proj = MolEncoder()
             
         for i in range(num_layers):
             self.layers.append(MessagePassing(hidden_dim, hidden_dim, dropout=dropout))
@@ -82,17 +108,12 @@ class GEncoder(nn.Module):
     def forward(self, data):
         x, edge_index, edge_attr, batch = data.x, data.edge_index, data.edge_attr, data.batch
 
-        x = self.input_proj(x)
-        
+        x, edge_attr = self.input_proj(x, edge_attr)
         for conv, norm, ffn in zip(self.layers, self.norms, self.ffn):
-            x_res = x 
             
-            x_new = conv(x, edge_index, edge_attr)
-            x = norm(x_res + x_new)
+            x = conv(x, edge_index, edge_attr)
+            x = norm(x)
             
-            x_res = x
-            x_new = ffn(x)
-            x = norm(x_res + self.dropout_layer(x_new))
             
         x_dense, mask = to_dense_batch(x, batch)
         z_graph = self.gap_proj(x_dense) 
@@ -105,65 +126,3 @@ class GEncoder(nn.Module):
 
         return z_graph
 
-
-class GEncoder2(nn.Module):
-    def __init__(self, num_layers=3, in_dim=node_feat_dim, hidden_dim=hidden_dim, dropout=0.1):
-        super(GEncoder2, self).__init__()
-
-        self.layers = nn.ModuleList()
-        self.norms = nn.ModuleList()
-        self.ffn = nn.ModuleList() 
-        self.dropout_layer = nn.Dropout(dropout)
-        
-        if in_dim != hidden_dim:
-            self.input_proj = nn.Linear(in_dim, hidden_dim)
-        else:
-            self.input_proj = nn.Identity()
-
-        for i in range(num_layers):
-            self.layers.append(AttMessagePassing(hidden_dim, hidden_dim, dropout=dropout))
-            self.norms.append(nn.RMSNorm(hidden_dim))
-            self.ffn.append(nn.Sequential(
-                nn.Linear(hidden_dim, hidden_dim),
-                nn.ReLU(),
-                nn.Linear(hidden_dim, hidden_dim)
-            ))
-            
-        self.gap_proj = nn.Linear(hidden_dim, hidden_dim)
-        self.att_vec = nn.Linear(hidden_dim, 1)
-        self.softmax = nn.Softmax(dim=1) 
-
-        self.projection_head = nn.Sequential(
-            nn.Linear(hidden_dim, projection_dim),
-            nn.RMSNorm(projection_dim),
-            nn.ReLU(),
-            self.dropout_layer,
-            nn.Linear(projection_dim, projection_dim)
-        )
-
-
-    def forward(self, data):
-        x, edge_index, edge_attr, batch = data.x, data.edge_index, data.edge_attr, data.batch
-        
-        x = self.input_proj(x)
-        
-        for conv, norm, ffn in zip(self.layers, self.norms, self.ffn):
-            x_res = x 
-            
-            x_new = conv(x, edge_index, edge_attr)
-            x = norm(x_res + x_new)
-            
-            x_res = x
-            x_new = ffn(x)
-            x = norm(x_res + self.dropout_layer(x_new))
-
-        x_dense, mask = to_dense_batch(x, batch)
-        z_graph = self.gap_proj(x_dense) 
-        att = self.att_vec(z_graph) 
-        alpha = self.softmax(att) 
-        h_graph = torch.sum(z_graph * alpha * mask.unsqueeze(-1), dim=1) 
-        
-        # 4. Projection finale
-        z_graph = self.projection_head(h_graph)
-
-        return z_graph
